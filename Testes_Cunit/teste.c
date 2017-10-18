acdbb4e5ccc137eee63ee14c18af0e1f42c73965
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-10-18 09:08:07 -0300

    teste feitos, podendo melhorar

--- Testes_Cunit/teste.c
+++ Testes_Cunit/teste.c
@@ -1,6 +1,6 @@
 #include <CUnit/Basic.h>
 //https://mysnippets443.wordpress.com/2015/03/07/ubuntu-install-cunit/
-// gcc  -o test test.c  -lcunit
+// gcc  -o teste teste.c  -lcunit
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -25,6 +25,32 @@ list_t* createList(){
   return list;
 }
 
+bool isEmpty(list_t *list){
+    return (list->size == 0);
+}
+
+int pop(list_t *list){
+    if(!isEmpty(list)){
+      int aux = list->head->item;
+      node_t *pointer = list->head;
+
+      list->head = pointer->next;
+      free(pointer);
+      list->size--;
+      return aux;
+    }
+    return -11;
+}
+
+int checkSize(list_t *list){
+  if(list != NULL){
+    return list->size;
+  }
+
+  return -11;
+}
+
+
 void push(list_t* list, int item){
   node_t* new_node = (node_t*) malloc(sizeof(node_t));
   new_node->item = item;
@@ -33,186 +59,91 @@ void push(list_t* list, int item){
   list->size++;
 }
 
-void printList(list_t *list){
-  node_t *pointer = list->head;
+// teste da hashTest
 
-  if(pointer == NULL) printf("Lista Vazia\n");
+#define HASH_SIZE 100
 
-  while(pointer != NULL){
-    printf("%d ", pointer->item);
-    pointer = pointer->next;
+typedef struct _element{
+  int value;
+} element_t;
 
-  }
-  printf("\n");
-}
+typedef struct _hashTable{
+  element_t *table[HASH_SIZE];
+  int sizeTable;
+} hashTable_t;
 
-bool isEmpty(list_t *list){
-    return (list->size == 0);
-
-    return false;
-}
-
-int pop(list_t *list){
-    if(!isEmpty(list)){
-      node_t *pointer = list->head;
-
-      list->head = pointer->next;
-      free(pointer);
-      list->size--;
-      return list->head->item;
-    }
-    return -1;
-}
-
-node_t* atPos(list_t *list, int index){
-
-  if(index >= 0 && index < list->size){
-    node_t* node = list->head;
-
-    int i;
-    for(i = 0; i < index; i++) node = node->next;
-
-    return node;
-
-  }
-  printf("Indice Invalido\n");
-  return NULL;
-}
-
-int indexOf(list_t *list, node_t *node){
-  if(node != NULL){
-      node_t *pointer = list->head;
-
-      int index = 0;
-      while(pointer != node && pointer != NULL){
-        pointer = pointer->next;
-        index++;
-      }
-      if(pointer != NULL) return index;
-  }
-
-  printf("No nao percentece a lista\n");
-  return -1;
-}
-
-void erase(list_t *list, int index){
-  if(index == 0){
-    pop(list);
-
-  } else{
-    node_t *current = atPos(list, index);
-
-    if(current != NULL){
-      node_t *previous = atPos(list, index - 1);
-      previous->next = current->next;
-
-      free(current);
-      list->size--;
-
-    }
-  }
-}
-
-void insert(list_t *list, int item, int index){
-  if(index == 0){
-    push(list, item);
-  }else{
-    node_t *current = atPos(list, index);
-
-    if(current != NULL){
-      node_t * previous = atPos(list, index - 1);
-      node_t *new_node = (node_t*) malloc(sizeof(node_t));
-      new_node->item = item;
-
-      previous->next = new_node;
-      new_node->next = current;
-      list->size++;
-    }
-  }
-}
-
-void xchgNodes(list_t *list, node_t *nodeA, node_t *nodeB){
-
-  if(nodeA == nodeB) return;
-
-  int indexA = indexOf(list, nodeA);
-  int indexB = indexOf(list, nodeB);
-
-  if(indexA > indexB){
-    nodeA = nodeB;
-    nodeB = atPos(list, indexA);
-
-    indexA = indexB;
-    indexB = indexOf(list, nodeB);
-  }
-
-  node_t *pb = atPos(list, indexB - 1);
-
-  if(nodeA == list->head){
-    list->head = nodeB;
-  }else{
-    node_t *pa = atPos(list, indexA - 1);
-    pa->next = nodeB;
-  }
-
-  pb->next = nodeA;
-
-  node_t *pointer = nodeA->next;
-  nodeA->next = nodeB->next;
-  nodeB->next = pointer;
-}
-
-node_t *min(list_t *list, int index){
-  node_t *pointer = atPos(list, index);
-
-  if(pointer != NULL){
-    node_t* minNode = pointer;
-
-    while(pointer != NULL){
-      if(pointer->item < minNode->item) minNode = pointer;
-
-      pointer = pointer->next;
-    }
-    return minNode;
-  }
-
-  return NULL;
-}
-
-node_t *max(list_t *list, int index){
-  node_t *pointer = atPos(list, index);
-
-  if(pointer != NULL){
-    node_t* maxNode = pointer;
-
-    while(pointer != NULL){
-      if(pointer->item > maxNode->item) maxNode = pointer;
-
-      pointer = pointer->next;
-    }
-    return maxNode;
-  }
-
-  return NULL;
-}
-
-void incSort(list_t *list){
+hashTable_t* createHashTable(){
+  hashTable_t *new_hashTable = (hashTable_t*) malloc(sizeof(hashTable_t));
   int i;
+  new_hashTable->sizeTable = 0;
 
-  for(i = 0; i < list->size - 1; i++) xchgNodes(list, atPos(list, i), min(list, i));
+  for(i = 0; i < HASH_SIZE; i++) new_hashTable->table[i] = NULL;
+
+  return new_hashTable;
 }
 
-void decSort(list_t *list){
-  int i;
+int put(hashTable_t *ht,int value, int position){
+  element_t *new_element = (element_t*) malloc(sizeof(element_t));
 
-  for(i = 0; i < list->size - 1; i++) xchgNodes(list, atPos(list,i), max(list,i));
+  new_element->value = value;
+  ht->table[position] = new_element;
+  ht->sizeTable++;
+  return new_element->value;
 }
 
-int checkSize(list_t *list){
-  if(list != NULL){
-    return list->size;
+int checkHashSize(hashTable_t *ht){
+  if(ht != NULL){
+    return ht->sizeTable;
+  }
+  return -11;
+}
+
+// tree test
+
+typedef struct _binaryTree {
+  int item;
+  int nodes;
+  struct _binaryTree *left;
+  struct _binaryTree *right;
+} binaryTree_t;
+
+binaryTree_t* createEmptyBinaryTree(){
+  return NULL;
+}
+
+binaryTree_t* createBinaryTree(int item, binaryTree_t *left, binaryTree_t *right){
+  binaryTree_t *new_tree = (binaryTree_t*) malloc(sizeof(binaryTree_t));
+
+  new_tree->nodes = 1;
+  new_tree->item = item;
+  new_tree->left = right;
+  new_tree->right = left;
+
+  return new_tree;
+}
+
+bool isEmptyy(binaryTree_t *bt){
+  if(bt == NULL) return true;
+  else return false;
+}
+
+binaryTree_t* add(binaryTree_t *bt, int item){
+  if(bt == NULL){
+    bt = createBinaryTree(item, NULL, NULL);
+  }else if(bt->item > item){
+    bt->left = add(bt->left, item);
   }else{
-    return -1;
+    bt->right = add(bt->right, item);
+  }
+  bt->nodes++;
+  return bt;
+}
+
+int checkSizeTree(binaryTree_t *bt){
+  if(bt == NULL){
+    return 0;
+  }else{
+    return bt->nodes;
   }
 }
 
@@ -228,49 +159,97 @@ int clean_suite(void)
 
 void listTest(){
 
-    list_t* list = createList();
+    list_t *list = createList();
 
-    CU_ASSERT_EQUAL(isEmpty(list), 0);
     push(list, 10);
-    printf("\n%d\n", checkSize(list));
 
-    CU_ASSERT_EQUAL(checkSize(list), 1);
-    push(list, 20);
+    CU_ASSERT(checkSize(list) == 1);
 
-    CU_ASSERT_EQUAL(checkSize(list), 2);
 
-    CU_ASSERT_EQUAL(pop(list), 20);
+    CU_ASSERT(pop(list) == 10);
 
-    CU_ASSERT_EQUAL(pop(list), 10);
+    CU_ASSERT(checkSize(list) == 1);
 
-    CU_ASSERT_EQUAL(pop(list), -1);
+    CU_ASSERT(checkSize(list) == 0);
+
+
 
 }
 
-/*
+
 void hashTest(){
 
+  hashTable_t *ht = createHashTable();
+
+  CU_ASSERT(checkHashSize(ht) == 0);
+
+  CU_ASSERT(put(ht, 2, 1) == 1);
+
+  CU_ASSERT(checkHashSize(ht) == 0);
+
+  CU_ASSERT(checkHashSize(ht) == 1);
+
+  CU_ASSERT(put(ht, 1, 2) == 1);
+
+  CU_ASSERT(checkHashSize(ht) == 2);
+
 }
 
-void binaryTreeTest(){
+void treeTest(){
+
+  binaryTree_t *bt = createEmptyBinaryTree();
+
+  bt = createBinaryTree(10, NULL, NULL);
+
+  CU_ASSERT(checkSizeTree(bt) == 1);
+
+  add(bt, 14);
+
+  CU_ASSERT(checkSizeTree(bt) == 2);
+
+  add(bt, 19);
+
+  CU_ASSERT(checkSizeTree(bt) == 3);
+
 
 }
-*/
+
 int main(){
-    CU_pSuite pSuite = NULL;
+  /* pSuite1 = lista
+     pSuite2 = hash
+     pSuite3 = Árvore
+ */
+
+    CU_pSuite pSuite1 = NULL;
+
     if(CUE_SUCCESS != CU_initialize_registry() )
     {
         return CU_get_error();
     }
 
-    pSuite = CU_add_suite("list_test_suite", init_suite, clean_suite);
-    if (NULL == pSuite)
+    // Adiciona suite1 para registro
+    pSuite1 = CU_add_suite("list_test_suite", init_suite, clean_suite);
+    if (NULL == pSuite1)
     {
         CU_cleanup_registry();
         return CU_get_error();
     }
 
-    if(NULL == CU_add_test(pSuite, "list_test_suite", listTest)){
+    // Adiciona test1 para suite1
+    if(NULL == CU_add_test(pSuite1, "list_test_suit", listTest)){
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+
+    // Adiciona test2 para suite2
+    if(NULL == CU_add_test(pSuite1, "hash_test_suit", hashTest)){
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // Adiciona test1 para suite1
+    if(NULL == CU_add_test(pSuite1, "tree_test_suit", treeTest)){
         CU_cleanup_registry();
         return CU_get_error();
     }
