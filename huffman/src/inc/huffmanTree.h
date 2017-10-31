8754fa0a3e42142bc70c80d0db5411ff2bfc5fb1
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-10-31 00:30:25 -0300

    novos comentarios na tree

--- huffman/src/inc/huffmanTree.h
+++ huffman/src/inc/huffmanTree.h
@@ -7,34 +7,76 @@
 /*
   Define the huffman tree type.
 */
-
 typedef struct _huffmanTree huffmanTree_t;
 
-
+/*
+   Creates a new tree.
+*/
 huffmanTree_t *createBinaryTree();
 
-
+/*
+   Destroy the tree and sets to NULL.
+   Input: The tree pointer pointer (huffmanTree_t **) to be destroyed.
+   Output: If the tree passed is NULL the function does nothing else destroy the tree.
+   Returns nothing because is a void function.
+*/
 void destroyBinaryTree(huffmanTree_t **);
 
-
+/*
+   Returns the number of nodes of a tree.
+   Input: The tree (huffmanTree_t *) which is desired to know the number of nodes.
+   Output: Returns -1 if the tree passed is NULL or the real value.
+ */
 short int numberOfNodes(huffmanTree_t *);
 
-
+/*
+   Returns the root of a tree.
+   Input: The tree (huffmanTree_t *) which the root is desired.
+   Output: Returns NULL if the tree passed is NULL or the tree's root node.
+*/
 node_t *root(huffmanTree_t *);
 
-
+/*
+   Puts the node in the tree, AS LIST AND IN A ORDERED WAY.
+   Input: The tree (huffmanTree_t *) which the node will be put and the node (node_t *).
+   Output: If one of the arguments passed is NULL, the function does nothing else put the
+   node in the tree.
+*/
 void add(huffmanTree_t *, node_t *);
 
-
+/*
+    
+*/
 void forest(huffmanTree_t *);
 
+/*
 
+*/
 node_t *getMin(huffmanTree_t *);
 
+/*
+   Sets the string that are the new codes of the itens of each leaf.
+   Input: The tree (huffmanTree_t *) which the new leaf codes will be set.
 
+   IMPORTANT ISSUE !!!
+      The tree must already have passed through the forest function.
+
+   Output: The string is set. Example: b'001100110.
+   Returns nothing because is a void function.
+*/
 void setLeaf(huffmanTree_t *);
 
+/*
+   Saves the nodes items from a tree in preorder to a file.
+   Input: The tree (huffmanTree_t *) and the file (FILE *) to be saved.
 
+      IMPORTANT ISSUE !!!
+         The tree must already have passed through the forest function.
+
+   Output: This function does nothing if one of the arguments, the tree or the file is NULL else
+   saves the items in the file.
+   Returns nothing because is a void function.
+*/
 void saveHuffmanTree(huffmanTree_t *, FILE *);
 
 #endif
