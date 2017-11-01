1da29b0394fe0bb3844f0357518b91e4de847b43
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-11-01 11:03:31 -0300

    headers comentados

--- huffman/src/inc/compress.h
+++ huffman/src/inc/compress.h
@@ -5,28 +5,47 @@
 
 #define COMPRESS_H
 
-/*  Codifica os bytes de um arquivo para um novo arquivo com o mesmo nove adicionado o .huff no final.
-    Entrada: O nome (char *) do arquivo a ser comprimido.
+/*
+    Compress the bytes of a file and save it in a file with the same name added the ".huff" extension.
+    Input: The file name (char *) to be compressed.
+    Output: If one or more arguments passed is NULL, the function does nothing else
+    the function will compress the file and adds ".huff" extension.
+    Returns nothing because is a void function.
 */
 void compress(char *);
 
-/*  Pega a frequência de cada byte do arquivo e salva na tabela hash.
-    Entrada: O arquivo (FILE *) e a tabela hash a ser salva as frequências (hashTablet_t *);
+/*
+    Catch the frequency of each byte of a file and saves it in a hash table.
+    Input: The source file (FILE *) and the hash table to save the frequency.
+    Output: If one or more arguments passed is NULL, the function does nothing else
+    save the frequencies bytes in the hash.
+    Returns nothing because is a void function.
 */
 void getFrequency(FILE *, hashTable_t *);
 
-/*  Pega cada item da tabela hash e transforma em uma árvore estritamente binária.
-    Entrada: A árvore binária (binaryTree_t *) onde os nós serão postos e a tabela hash (hashTablet_t *) onde os nós estão.
+/*
+    Catch each item from the hash table and transforms it in a tree strictly binary tree.
+    Input: The tree (huffmanTree_t *) to be used and the hash table (hashTable_t *) with the nodes.
+    Output: If one or more arguments passed is NULL, the function does nothing else transforms the hash table elements
+    in a strictly binary tree.
+    Returns nothing because is a void function.
 */
 void hashTree(huffmanTree_t *, hashTable_t *);
 
 /*  Salva o arquivo usando a nova codificação para cada byte.
     Entrada: O arquivo original (FILE *), o arquivo final (FILE *), onde será salvo, a tabela hash (hashTablet_t *) que tem a nova codificação e por fim a árvore usada para gerar a nova codificação (binaryTree_t *).
+    Saves the file with the new codes.
+    Input: The source file (FILE *), the huffman file (FILE *),
+    the hash table (hashTable_t *) that hash the new codes and the tree used to generate the new codes.
+    Output: If one or more arguments passed is NULL, the function does nothing else save the new codes in the source file.
+    Returns nothing because is a void function.
 */
 void saveCompress(FILE *, FILE *, hashTable_t *, huffmanTree_t *);
 
-/* Mostra se o byte escolhido está setado.
-  Entrada: tipo do byte (unsigned char) e a position do bit (int t).
+/*
+   Shows if the byte picked was set.
+   Input: The byte type (unsigned char) and the position (short int).
+   Output: Returns the bit int the position picked.
 */
 unsigned char bitSet(unsigned char, short int t);
 
--- huffman/src/inc/descompress.h
+++ huffman/src/inc/descompress.h
@@ -5,21 +5,40 @@
 #define DESCOMPRESS_H
 
 
+/*
+   Descompress the Bytes of a file and save it to a new file with the same name
+   without ".huff" at the end.
+   Input: The name (char *) of the file to be descompressed.
+   Output: If the string passed is NULL or the file passed not exist, the function does nothing else
+   the funciton will descompress the file with ".huff" extension and erase the ".huff" extension.
+   Returns nothing because is a void function.
+ */
+
 void descompress(char *);
 
-/*  Essa função pega ou forma a árvore que fou usada para codificar o arquivo.
-    Entrada: A árvore binária (binaryTree_t *) para formar e o arquivo (FILE *) onde ela está.
+/*
+    This function catch/build the tree which was created to compress the file.
+    Input: The tree (huffmanTree_t *) to build and the source file (FILE *).
+    Output: If one or more arguments passed is NULL, the function does nothing else
+    the function will catch or build the binary tree.
+    Returns nothing because is a void function.
  */
 void getBinaryTree(huffmanTree_t *, FILE *, unsigned short int);
 
-/*  Salva o arquivo usando os bytes original.
-    Entrada: Arquivo codificado (FILE *), o arquivo final (FILE *), onde sera salvo e a árvore usada para gerar os códigos
-    de cada byte codificado.
+/*
+    Saves the file using the standard codes.
+    Input: The compress file (FILE *), the final file (FILE *) which will save and the tree with the huffman codes.
+    Output: If one or more arguments passed is NULL, the function does nothing else
+    the function will save the standard codes in the final file.
+    Returns nothing because is a void function.
  */
 void saveDecompress(FILE *, FILE *, huffmanTree_t *, unsigned short int);
 
-/*  Retorna true se o bit está setado.
-    Entrada: Um unsigned char e a posição a ser consultada.
+/*
+    Returns true if the i-th bit of a Byte is set and false otherwise.
+    Input: The base byte (unsigned char) and the position of the bit that you want to
+    know if it is set (short int), 0 for the first one on the right and 7 for the first one on the left.
+    Output: true if is set and false if not.
  */
 bool isBitSet(unsigned char, short int);
 
