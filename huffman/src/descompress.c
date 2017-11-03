cda2f72c8e86b73aefaf2f3785a13b4ef8a60c1c
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-11-02 21:52:48 -0300

    descompress done!

--- huffman/src/descompress.c
+++ huffman/src/descompress.c
@@ -1,132 +1,158 @@
 #include "inc/descompress.h"
 
-void descompress(char *to_decode_name){
+void descompress(char *toDescompressFile){ // Enter with the file to be descompressed.
 
-	if (to_decode_name != NULL) {
-		FILE *to_decode = NULL;
-		FILE *decoded = NULL;
-
-
-		to_decode = fopen(to_decode_name, "rb");
-
-		if (to_decode != NULL) {
-
-			unsigned short int header;
-
-			unsigned short int trash;
-
-			unsigned short int tree_size;
+	if (toDescompressFile != NULL) { // check if the file is valid.
+		FILE *toDescompress = NULL; // Creates toDescompress file.
+		FILE *descompress = NULL; // Creates descompress file.
 
 
-			huffmanTree_t *tree = createBinaryTree();
+		toDescompress = fopen(toDescompressFile, "rb"); // Read the file in "rb" mode, opening non-text files, that is the binaries.
 
-			char *decoded_name = (char *) calloc(strlen(to_decode_name),sizeof(char));
+		if (toDescompress != NULL) { // check if the file is ok.
 
-			strcpy(decoded_name, to_decode_name);
+			unsigned short int header; // The two first bytes.
 
+			unsigned short int trash; // The trash bits.
 
-			decoded_name[strlen(to_decode_name) - 5] = '\0';
-
-
-			decoded = fopen(decoded_name, "wb");
-
-			fread(&header, sizeof(unsigned short int), 1, to_decode);
+			unsigned short int tree_size; // The tree size.
 
 
-			trash = header >> 13;
+			huffmanTree_t *tree = createBinaryTree(); // creates the tree.
 
-			tree_size = header << 3;
+			/*
+			   Allocates memory for the string to put the name of
+			   the output file, which is the input file
+			   name without ".huff" at the end.
+			 */
+			char *descompressedFile = (char *) calloc(strlen(toDescompressFile),sizeof(char));
 
-			tree_size = tree_size >> 3;
+			strcpy(descompressedFile, toDescompressFile); // Copies the source string to descompressedFile.
 
-			printf("\nDescomprimindo: %s\n", to_decode_name);
-			printf("\nFormando a arvore...\n");
+			descompressedFile[strlen(toDescompressFile) - 5] = '\0'; // Takes off the extension ".huff",
 
-			getBinaryTree(tree, to_decode, tree_size);
-			printf("\n\tPronto\n");
-			printf("\nDescomprimindo...\n\n");
+			descompress = fopen(descompressedFile, "wb"); // Write the file in "wb" mode, writing the binaries to descompressed file.
 
-			saveDecompress(to_decode, decoded, tree, trash);
-			printf("\nArquivo Descomprimido com Sucesso\n\n");
+			fread(&header, sizeof(unsigned short int), 1, toDescompress); // Reads the header.
 
-			fclose(to_decode);
-			fclose(decoded);
+			trash = header >> 13; // gets the trash.
 
-			free(decoded_name);
+			tree_size = header << 3; // gets the bits which has the tree size information.
 
-			destroyBinaryTree(&tree);
+			tree_size = tree_size >> 3; // Adjust the bytes.
+
+			printf("\nDescompressing: %s !!!\n", toDescompressFile);
+			printf("\nForming the tree...\n");
+
+			getBinaryTree(tree, toDescompress, tree_size); // This function catch/build the tree which was created to compress the file.
+			printf("\nFirst Steps Ok!\n");
+			printf("\nnDescompressing...\n\n");
+
+			saveDecompress(toDescompress, descompress, tree, trash); // Saves the file using the standard codes.
+			printf("\nFile Descompressed!!!!!\n\n");
+
+			fclose(toDescompress); //  closes the stream. All buffers are flushed.
+			fclose(descompress); //  closes the stream. All buffers are flushed.
+
+			free(descompressedFile); // free memory.
+
+			destroyBinaryTree(&tree); // destroy the tree and pointer to NULL.
 		} else {
-			printf("\nO arquivo %s não foi encontrado.\n\n", to_decode_name);
+			printf("\nhe file %s wasn't found.\n\n", toDescompressFile);
 		}
 	}
 }
 
+/*
+    This function catch/build the tree which was created to compress the file.
+
+		MORE ABOUT THIS FUNCTION: open "descompress.h" file.
+*/
 void getBinaryTree(huffmanTree_t *tree, FILE *src, unsigned short int tree_size)
 {
 
-	if (tree != NULL && src != NULL) {
+	if (tree != NULL && src != NULL) { // check if the arguments are valid.
 
-		if (tree_size > 1) {
-			add(tree, getNode(src));
+		if (tree_size > 1) { // if the tree_size is greater than one, adds the returned node by getNode(See node.h to more information) in the tree.
+			add(tree, getNode(src)); // Puts the node in the tree, AS LIST AND IN A ORDERED WAY.
 		} else {
-
-			add(tree, createNode(AUX, 0, getNode(src), NULL));
+			/*
+				If the tree_size isn't greather than one,
+				adds the node which is the auxiliary item "*" with only the left child
+				which is returned by getNode.
+			*/
+			add(tree, createNode(AUX, 0, getNode(src), NULL)); // Puts the node in the tree, AS LIST AND IN A ORDERED WAY.
 		}
 	}
 }
 
+/*
+    Saves the file using the standard codes.
+
+		MORE ABOUT THIS FUNCTION: open "descompress.h" file.
+*/
 void saveDecompress(FILE *src, FILE *dest, huffmanTree_t *tree, unsigned short int trash)
 {
 
-	if (src != NULL && dest != NULL && tree != NULL) {
-		unsigned char buffer;
+	if (src != NULL && dest != NULL && tree != NULL) { // check if the arguments are valid.
+		unsigned char buffer; // the buffer which will used to reads the datas from the source.
 
-		node_t *tmp = root(tree);
+		node_t *tmp = root(tree); // Starts in the root of the tree.
+
+		fread(&buffer, sizeof(unsigned char), 1, src); // Reads the first byte data from src into the unsigned char buffer.
 
 
-		fread(&buffer, sizeof(unsigned char), 1, src);
-
-
-		while (!feof(src)) {
+		while (!feof(src)) { // checks the loop until the end-of-file.
 			unsigned char aux;
 
-
+			/*
+			  Tries to read the byte that follow the last read
+			  Byte, it serves to know if the last Byte read was the
+			  last one of the file, only in that case we consider
+			  the amount of "trash bits".
+			*/
 			fread(&aux, sizeof(unsigned char), 1, src);
 
+      short int i; // loop aux.
+			for (i = 7; i >= 0; i--) { // checks each bit of the read file.
 
-       short int i;
-			for (i = 7; i >= 0; i--) {
-
+				/*
+					To do not check the "trash bits", if the
+					end-of-file flag was not raised we check all
+					the bits of the read byte, but if it was
+					raised then we only check until the last bit
+					before the "trash bits".
+				*/
 				if (!feof(src) || trash <= i) {
 
-					if (isBitSet(buffer, i)) {
-
-						tmp = right(tmp);
+					if (isBitSet(buffer, i)) { // checks if the bit is set or not.
+						tmp = right(tmp); // if true puts in the right, like the huffman algorithm demands.
 					} else {
-
-						tmp = left(tmp);
+						tmp = left(tmp); // if false, its "0", so puts in the left node.
 					}
 
+					if (isLeaf(tmp)) { // check if tmp is a leaf (no children nodes).
 
-					if (isLeaf(tmp)) {
+						unsigned char aux_tmp = item(tmp); // gets the item from the leaf.
 
-						unsigned char aux_tmp = item(tmp);
+						tmp = root(tree); // goes back to the root.
 
-						tmp = root(tree);
-
-
-						fwrite(&aux_tmp, sizeof(unsigned char) , 1, dest);
+						fwrite(&aux_tmp, sizeof(unsigned char) , 1, dest); // wrties the leaf byte to the dest file.
 					}
 				}
 
 			}
 
-
-			buffer = aux;
+			buffer = aux; // Pass the byte previously read "aux" to the buffer.
 		}
 	}
 }
 
+/*
+    Returns true if the i-th bit of a Byte is set and false otherwise.
+
+		MORE ABOUT THIS FUNCTION: open "descompress.h" file.
+*/
 bool isBitSet(unsigned char base, short int i){
 
 	unsigned char mask = 1 << i;
