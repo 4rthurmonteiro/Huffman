84c5834cb706e159ce13bf7bdbb77843379567bc
Arthur Monteiro Alves Melo <monteiroamelo@gmail.com>
2017-11-03 01:19:36 -0300

    tree coments done!

--- huffman/src/huffmanTree.c
+++ huffman/src/huffmanTree.c
@@ -25,21 +25,28 @@ struct _huffmanTree {
 */
 huffmanTree_t *createBinaryTree(){
 
-	huffmanTree_t *new_huff = (huffmanTree_t *) malloc(sizeof(huffmanTree_t));
+	huffmanTree_t *new_huff = (huffmanTree_t *) malloc(sizeof(huffmanTree_t)); // Allocates memory to the new tree.
 
+	new_huff->size = malloc(sizeof(short int)); // Allocates memory to the size value.
 
-	new_huff->size = malloc(sizeof(short int));
-	short int aux = 0;
-	memcpy(new_huff->size, &aux, sizeof(short int));
+	short int aux = 0; // auxiliary short int, to save "0" to the new_huff->size.
 
-	new_huff->nodes = malloc(sizeof(short int));
-  short int aux2 = 0;
-	memcpy(new_huff->nodes, &aux2, sizeof(short int));
+	memcpy(new_huff->size, &aux, sizeof(short int)); // puts "0" in the new_huff->size.
 
-	new_huff->head = NULL;
-	new_huff->tail = NULL;
+	new_huff->nodes = malloc(sizeof(short int)); // Allocates memory to the nodes value.
 
+  short int aux2 = 0; // auxiliary short int, to save "0" to the new_huff->nodes.
 
+	memcpy(new_huff->nodes, &aux2, sizeof(short int));// puts "0" in the new_huff->nodes.
+
+	new_huff->head = NULL; // pointer head to NULL.
+
+	new_huff->tail = NULL; // pointer tail to NULL.
+
+/*
+		Returns the tree, but its important to remember that's still a linked list
+		and will be transformed in a strictly binary tree when pass to the function forest.
+*/
 	return new_huff;
 }
 
@@ -50,24 +57,22 @@ huffmanTree_t *createBinaryTree(){
 */
 void destroyBinaryTree(huffmanTree_t **tree){
 
-	if ((*tree) != NULL) {
+	if ((*tree) != NULL) { // check if the arguments are valid.
 
-		node_t *tmp;
+		node_t *tmp; // auxiliar node.
 
+		while ((*tree)->head != NULL) { // loop checking if the node->head is still valid.
 
-		while ((*tree)->head != NULL) {
+			tmp = next((*tree)->head); // save the next node.
 
-			tmp = next((*tree)->head);
+			destroyNode(&(*tree)->head); // destroy the head node and puts do NULL.
 
-			destroyNode(&(*tree)->head);
-
-			(*tree)->head = tmp;
+			(*tree)->head = tmp; // now the head is the second node at the beginnig.
 		}
 
+		free((*tree)); // free memory.
 
-		free((*tree));
-
-		(*tree) = NULL;
+		(*tree) = NULL; // and puts the tree to NULL.
 	}
 }
 
@@ -78,12 +83,11 @@ void destroyBinaryTree(huffmanTree_t **tree){
 */
 short int numberOfNodes(huffmanTree_t *tree){
 
-	if (tree != NULL) {
+	if (tree != NULL) { // check if the arguments are valid.
 
-		return (*(short int*)tree->nodes);
+		return (*(short int*)tree->nodes); // It's necessary make a cast in the variable because its *void type.
 	} else {
-
-		return -1;
+		return -1; // logical error.
 	}
 }
 
@@ -94,9 +98,9 @@ short int numberOfNodes(huffmanTree_t *tree){
 */
 node_t *root(huffmanTree_t *tree){
 
-	if (tree != NULL) {
+	if (tree != NULL) { // check if the arguments are valid.
 
-		return tree->head;
+		return tree->head; // returns the root.
 	} else {
 
 		return NULL;
@@ -111,60 +115,83 @@ node_t *root(huffmanTree_t *tree){
 void add(huffmanTree_t *tree, node_t *to_add)
 {
 
-	if (tree != NULL && to_add != NULL) {
+	if (tree != NULL && to_add != NULL) { // check if the arguments are valid.
 
-		if ((*(int*)tree->size) == 0) {
-			tree->head = to_add;
-			tree->tail = to_add;
+		if ((*(int*)tree->size) == 0) { // checks if the tree is empty. It's necessary make a cast because size is a *void type.
+
+			tree->head = to_add; // The tree (still is a list) is empty so the node added is the head of the list.
+
+			tree->tail = to_add; // The tree (still is a list) is empty so the node added is the tail of the list.
 		} else {
 
-			node_t *tmp = tree->head;
+			node_t *tmp = tree->head; // auxiliary node.
 
 
-			while (tmp != NULL && frequency(tmp) < frequency(to_add)) {
-				tmp = next(tmp);
+			while (tmp != NULL && frequency(tmp) < frequency(to_add)) { // searches the first node with frequency higher.
+				tmp = next(tmp); // it's not higher so go to the next node.
 			}
 
+			/*
+				After the while the next node have frequency greater of the to_add (node_t).
+			*/
+			nextSet(to_add, tmp);  // Sets the next node.
 
-			nextSet(to_add, tmp);
+			if (tmp != NULL) { // If that we are adding does not have the highest frequency so far.
 
-
-			if (tmp != NULL) {
-
+				/*
+					 We say that the previous Node to which we are
+				   adding is the Node that was previous of the
+				   one that has a higher frequency than the one
+				   we are adding.
+				 */
 				previouSet(to_add, previous(tmp));
 
 
-				if (tmp == tree->head) {
+				if (tmp == tree->head) { // if is the lowest frequency so far.
 
-					tree->head = to_add;
+					tree->head = to_add; // now is the head of the list.
 				} else {
 
+					/*
+					  	If not, we say that the Node that
+					   follow the one that has turn into the
+					   one that is before to the one we are
+					   adding is the one we are adding.
+					 */
 					nextSet(previous(to_add), to_add);
 				}
 
 
+				/*
+						We say that the Node before to the Node that
+				  	has turned into the next to the one we are
+				  	adding is the one we are adding.
+				 */
 				previouSet(next(to_add), to_add);
 			} else {
-
+				/*
+						If it has the highest frequency so far, we
+				  	say the previous to the one we are adding is
+				  	the list "Tail".
+				 */
 				previouSet(to_add, tree->tail);
 
-				nextSet(tree->tail, to_add);
 
+				nextSet(tree->tail, to_add); // That the following to the list "tail" is the one we are adding.
 
-				tree->tail = to_add;
+				tree->tail = to_add; // the new list tail.
 			}
 		}
 
-		short int intSize = (*(short int*)tree->size);
-		intSize++;
+		short int intSize = (*(short int*)tree->size); // puts the tree->size with cast (*void type) into a auxiliary variable.
+		intSize++; // adds "1" to the aux variable.
 
-		memcpy(tree->size, &intSize, sizeof(short int));
-		//tree->size++;// vai dar pau
+		memcpy(tree->size, &intSize, sizeof(short int)); // and puts into the tree->size this value updated.
 
-		short int intNodes = (*(short int*)tree->nodes);
-		intNodes++;
-		memcpy(tree->nodes, &intNodes, sizeof(short int));
-		//tree->nodes++;//vai dar pau
+		short int intNodes = (*(short int*)tree->nodes); // puts the tree->nodes with cast (*void type) into a auxiliary variable.
+		intNodes++; // adds "1" to the aux variable.
+
+		memcpy(tree->nodes, &intNodes, sizeof(short int)); // and puts into the tree->nodes this value updated.
 	}
 }
 
@@ -175,22 +202,38 @@ void add(huffmanTree_t *tree, node_t *to_add)
 */
 void forest(huffmanTree_t *tree){
 
-	if (tree != NULL) {
+	if (tree != NULL) { // check if the arguments are valid.
 
-		while ((*(int*)tree->size) > 1) {
+		while ((*(int*)tree->size) > 1) { // checks the loop until at least remains two elements in the tree.
 
+		 /*
+		 	 We remove the two nodes from the list whose item has
+		   the lowest frequency.
+		 */
 			node_t *left = getMin(tree);
 			node_t *right = getMin(tree);
 
-			if (left == NULL && right == NULL) {
+			if (left == NULL && right == NULL) { // if the both are NULL the function stop. The list is empty.
 
 				break;
-			} else if (right == NULL) {
+   				/*
+						If only the right is NULL, it means that
+				   there was only one Node in the list, so the
+				   frequency of the auxiliary Node we are going
+				   to add will be the same as the left child. *
+				 */
+			} else if (right == NULL) { // Only the right is NULL so just one more node in the list.
 
-				add(tree,	createNode(AUX, frequency(left), left, right));
+				add(tree,	createNode(AUX, frequency(left), left, right)); // add the "*" node with te frequency equal with the left child.
+
+				/*
+					If neither is NULL, the frequency of the
+			    auxiliary Node we are going to add will be
+		      the sum of the left and right frequencies. *
+				 */
 			} else {
 
-				add(tree,	createNode(AUX, frequency(left) + frequency(right),	left, right));
+				add(tree,	createNode(AUX, frequency(left) + frequency(right),	left, right)); // add the "*" node with te frequency equal with the sum of the right and the left child.
 			}
 		}
 	}
@@ -204,36 +247,37 @@ void forest(huffmanTree_t *tree){
 */
 node_t *getMin(huffmanTree_t *tree){
 
-	if (tree != NULL) {
-		if ((*(int*)tree->size) == 0) {
+	if (tree != NULL) { // check if the arguments are valid.
+		if ((*(int*)tree->size) == 0) { // the list is empty so return NULL.
 
 			return NULL;
 		} else {
 
-			node_t *tmp = tree->head;
+			node_t *tmp = tree->head; // takes the head, because the list is ordered so the HEAD HAS THE LOWEST FREQUENCY.
 
-			tree->head = next(tree->head);
+			tree->head = next(tree->head); // The list head is now the second element of the list.
 
-			short int intSize = (*(short int*)tree->size);
-			intSize--;
+			short int intSize = (*(short int*)tree->size); // puts the tree->size with cast (*void type) into a auxiliary variable.
+			intSize--; // decreases "1".
 
-			memcpy(tree->size, &intSize, sizeof(short int));
-//			tree->size--;
+			memcpy(tree->size, &intSize, sizeof(short int)); // and puts into the tree->size this value updated.
 
-			if ((*(short int*)tree->size) != 0) {
+			if ((*(short int*)tree->size) != 0) { // Still have elements in the list.
 
-				previouSet(tree->head, NULL);
-			} else {
+				previouSet(tree->head, NULL); // so the previous of the new head is NULL.
+			} else { // No more elements in the list.
 
-				tree->tail = NULL;
+				tree->tail = NULL; // so the tail is NULL.
 			}
 
-
+			 /*
+				 It causes the next and the previous to the Node that
+			   we take out to be NULL. *
+			 */
 			nextSet(tmp, NULL);
 			previouSet(tmp, NULL);
 
-
-			return tmp;
+			return tmp; // returns the withdrawn Node.
 		}
 	} else {
 		return NULL;
@@ -247,18 +291,28 @@ node_t *getMin(huffmanTree_t *tree){
 */
 void setLeaf(huffmanTree_t *tree){
 
-	if (tree != NULL) {
+	if (tree != NULL) { // check if the arguments are valid.
 
-		if ((*(short int*)tree->nodes) == 1) {
+		if ((*(short int*)tree->nodes) == 1) { // just one type of item (byte) in the input.
 
-			representationSet(tree->head, "0");
+			representationSet(tree->head, "0"); // the new represent is 0 (MAX COMPRESS).
 		} else {
 
-			char *code = (char *) calloc(height(tree->head), sizeof(char));//vai dar merdaaaa
+			 /*
+				 Otherwise, allocates memory to a string that will
+			   have exactly the maximum size for any representation
+			   of the leaves of the Tree.
+			 */
+			char *code = (char *) calloc(height(tree->head), sizeof(char));
 
+			 /*
+				 Sets the representation of the root Node of the Tree,
+			   since this function is recursive it just sets in all
+			   and only in the leaves.
+			 */
 			setNode(tree->head, code);
 
-			free(code);
+			free(code); // free memory.
 		}
 	}
 }
@@ -270,8 +324,12 @@ void setLeaf(huffmanTree_t *tree){
 */
 void saveHuffmanTree(huffmanTree_t *tree, FILE *dest){
 
-	if (tree != NULL && dest != NULL) {
+	if (tree != NULL && dest != NULL) { // check if the arguments are valid.
 
+		/*
+			  We save the root Node of the Tree, since this function is
+		   recursive it saves all the nodes of the Tree in preorder. *
+		 */
 		saveNode(tree->head, dest);
 	}
 }
